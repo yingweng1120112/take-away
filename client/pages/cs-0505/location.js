cd18e2bb44278a8d375d45a96617ac5510c817a3
larry <asd109150@gmail.com>
2024-05-06 14:19:10 +0800

    location css減少

--- client/pages/cs-0505/location.js
+++ client/pages/cs-0505/location.js
@@ -54,7 +54,7 @@ export default function Location() {
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.9585094222593!2d120.22379571188887!3d23.025295579085018!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e771f2995cba5%3A0x3bfd449f1e46ffef!2z5Y2X6Ie656eR5oqA5aSn5a24!5e0!3m2!1szh-TW!2stw!4v1713363284909!5m2!1szh-TW!2stw"
               width={800}
-              height={600}
+              height={400}
               style={{ border: 0, borderRadius: '5%' }}
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
@@ -168,7 +168,7 @@ export default function Location() {
         </div>
       </section>
       <section className={styles['carousel']}>
-        <div className={styles['swiper mySwiper']}>
+        <div className={styles['swiper']}>
           <div className={styles['swiper-slide']}>
             <img src={`/img/about-us.jpg`} alt="" />
             <div className={styles['adopt-title']}>
