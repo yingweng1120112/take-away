8dd40eae5aa1c37fb16afba171c0f2c546c09dfd
yingweng <yingweng1120112@gmail.com>
2024-05-06 11:19:06 +0800

    origin

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,4 +1,4 @@
-// import '@/styles/globals.css'
+import '@/styles/global.css'
 // import '@/styles/product-table.css'
 //  3. 最外(上)元件階層包裹提供者元件，讓⽗⺟元件可以提供它:
 import { ThemeProvider } from '@/hooks/use-theme'
