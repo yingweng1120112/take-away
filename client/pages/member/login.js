12ea62f42ed7e796f394e5646fa936d430a869da
yingweng <yingweng1120112@gmail.com>
2024-05-06 10:15:46 +0800

    0506 origin

--- client/pages/member/login.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import LoginForm from '@/components/member/login-form'
-
-export default function Login() {
-  return <LoginForm />
-}
--- client/pages/member/register.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import RegisterForm from '@/components/member/register-form'
-
-export default function Register() {
-  return <RegisterForm />
-}
