950105ea92d7969bc05c4d27203c34eada75b2ae
yushanys <lightful2013@gmail.com>
2024-05-19 22:38:27 +0800

    暫存

---
+++ client/pages/shopping-cart/step1-copy.js
@@ -0,0 +1,180 @@
+import { useState, useEffect } from 'react'
+import styles from '@/styles/shopping-cart/shoppingcar-step1.module.css'
+import Carousel from '@/components/shopping-cart/carousel'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faSquareMinus,
+  faSquarePlus,
+  faTrashCan,
+  faStore,
+  faAnglesRight,
+} from '@fortawesome/free-solid-svg-icons'
+import { loadProducts } from '@/services/testproduct'
+
+export default function Step1() {
+  //所有可用的產品列表
+  const [products, setProducts] = useState([])
+  //購物車中的項目
+  const [items, setItems] = useState([])
+
+  const getProducts = async () => {
+    const data = await loadProducts()
+    // 確認資料結構是否與原始專案相符，並設置到狀態中
+    if (Array.isArray(data.product)) {
+      setProducts(data.product)
+    } else {
+      console.error('資料結構不符')
+    }
+    console.log(data)
+  }
+  useEffect(() => {
+    getProducts()
+  }, [])
+  return (
+    <>
+      <header />
+      {/* 背景樣式上 */}
+      <section className={`${styles['roof']} ${styles['sectionstyle']}`}>
+        <img src="/shopping-cart/roof.png" alt="" />
+      </section>
+      {/* 步驟 */}
+      <section className={`${styles['step']} ${styles['sectionstyle']}`}>
+        <div className={styles['stepbg']}>
+          <div className={styles['everystep']}>
+            <div className={styles['nextto']}>
+              <h1>1</h1>
+              <h2>購物車</h2>
+            </div>
+            <div className={styles['nextto']}>
+              <h1>2</h1>
+              <h2>填寫資料</h2>
+            </div>
+            <div className={styles['nextto']}>
+              <h1>3</h1>
+              <h2>訂單確認</h2>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* 購物車 */}
+      <div className={styles['shoppingcar']}>
+        <div className={styles['shoppingcarleft']}>
+          <div className={`${styles['carttitle']} ${styles['carttopstyle']}`}>
+            <div>購物車（{products.length}件）</div>
+          </div>
+          <div className={styles['cartdetail']}>
+            <div className={styles['cartdetailleft']}>
+              <input type="checkbox" /> 商品資料
+            </div>
+            <div className={styles['cartdetailright']}>
+              <div>優惠</div>
+              <div>單件價格</div>
+              <div>數量</div>
+              <div>小計</div>
+            </div>
+          </div>
+          <div className={styles['itemscroll']}>
+            {/* 商品列表 */}
+            {items.map((v, i) => {
+              return (
+                <div key={v.product_id} className={styles['cartitem']}>
+                  <div className={styles['cartitemleft']}>
+                    <div className={styles['carti']}>
+                      <input type="checkbox" />
+                    </div>
+                    <div className={styles['itemlist']}>
+                      <div className={styles['cartimg']}>
+                        <img src="/shopping-cart/car-item1.png" alt="" />
+                      </div>
+                      <div>{v.name}</div>
+                    </div>
+                  </div>
+                  <div className={styles['cartitemright']}>
+                    <div>0</div>
+                    <div>{v.price}</div>
+                    <div className={styles['itemamount']}>
+                      <FontAwesomeIcon
+                        icon={faSquarePlus}
+                        className={styles['iconstyle']}
+                        onClick={() => {}}
+                      />
+                      <div>1</div>
+                      <FontAwesomeIcon
+                        icon={faSquareMinus}
+                        className={styles['iconstyle']}
+                        onClick={() => {}}
+                      />
+                    </div>
+                    <div>$90</div>
+                    <div>
+                      <FontAwesomeIcon
+                        icon={faTrashCan}
+                        className={styles['iconstyle']}
+                        onClick={() => {}}
+                      />
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+        {/* 右側訂單資訊 */}
+        <div className={styles['shoppingcarright']}>
+          <div className={`${styles['carttitle']} ${styles['carttopstyle1']}`}>
+            <div>訂單資訊</div>
+          </div>
+          <div className={styles['cartbottomstyle']}>
+            <div>
+              <div>小計：</div>
+              <div>$270</div>
+            </div>
+            <div>
+              <div>運費：</div>
+              <div>$80</div>
+            </div>
+            <div>
+              <div>優惠：</div>
+              <div>$0</div>
+            </div>
+            <div>
+              <div>合計：</div>
+              <div>$350</div>
+            </div>
+            <div className={styles['cartbutton']}>
+              <button className={styles['buttonstyle']}>
+                <FontAwesomeIcon
+                  icon={faStore}
+                  className={styles['iconstyle1']}
+                />
+                繼續購物
+              </button>
+              <button className={styles['buttonstyle']}>
+                填寫資料
+                <FontAwesomeIcon
+                  icon={faAnglesRight}
+                  className={styles['iconstyle2']}
+                />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* 商品推薦 */}
+      <section className={styles['commend']}>
+        <h4>相關商品推薦</h4>
+        <div className={styles['carouselstyle']}>
+          <Carousel />
+        </div>
+      </section>
+
+      {/* 下花邊 */}
+      <section className={styles['wall']}>
+        <img src="/shopping-cart/wall.png" alt="" />
+      </section>
+      <footer></footer>
+    </>
+  )
+}
--- client/pages/shopping-cart/testproduct.js
+++ client/pages/shopping-cart/testproduct.js
@@ -6,11 +6,20 @@ import styles from '@/components/testproduct/cart.module.css'
 export default function List() {
   const [products, setProducts] = useState([])
 
+  // 加入到購物車的函數
+  const addProduct = (product) => {
+    const newProduct = { ...product, qty: 1 }
+    const nextProducts = [newProduct, ...products]
+    setProducts(nextProducts)
+  }
+
   const getProducts = async () => {
     const data = await loadProducts()
     // 確認資料結構是否與原始專案相符，並設置到狀態中
     if (Array.isArray(data.product)) {
       setProducts(data.product)
+    } else {
+      console.error('資料結構不符', data)
     }
     console.log(data)
   }
@@ -28,7 +37,8 @@ export default function List() {
             <Link href={`/shopping-cart/testproduct/${v.product_id}`}>
               {v.name}
             </Link>
-            <button>加入購物車</button>
+            {v.price}
+            <button onClick={() => addProduct(v)}>加入購物車</button>
           </li>
         ))}
       </ul>
