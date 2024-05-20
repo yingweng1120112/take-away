47b5e4b077984c85b3847e0cf267fe8d3bd7e8df
yingweng <yingweng1120112@gmail.com>
2024-05-20 11:21:50 +0800

    login完成，register還沒開始

--- client/pages/user/login-register.js
+++ client/pages/user/login-register.js
@@ -1,7 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '@/styles/user/login-register.module.scss'
 import Header from '@/components/layout/header'
 import Footer from '@/components/layout/footer'
+
+// 解析accessToken用的函式
+const parseJwt = (token) => {
+  const base64Payload = token.split('.')[1]
+  const payload = Buffer.from(base64Payload, 'base64')
+  return JSON.parse(payload.toString())
+}
 
 export default function LoginRegister() {
   useEffect(() => {
@@ -21,32 +28,208 @@ export default function LoginRegister() {
     }
   }, [styles])
 
+  // 記錄欄位輸入資料，狀態為物件，物件的屬性名稱要對應到欄位的名稱
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  })
+
+  // 記錄欄位錯誤訊息的狀態
+  const [errors, setErrors] = useState({
+    email: '',
+    password: '',
+  })
+
+  // 顯示密碼的核取方塊用
+  const [showPassword, setShowPassword] = useState(false)
+
+  // 多欄位共用事件處理函式
+  const handleFieldChange = (e) => {
+    // 可以利用e.target觀察目前是在輸入或操作哪個欄位上
+    console.log(e.target.name, e.target.type, e.target.value)
+    // ES6: computed property names (計算得來的屬性名稱)
+    // [e.target.name]: e.target.value }
+    // ^^^^^^^^^^^^^^^ 這裡可以動態的代入變數值或表達式，計算出物件屬性名稱(字串)
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer#computed_property_names
+    setUser({ ...user, [e.target.name]: e.target.value })
+  }
+
+  // 表單送出事件處理函式
+  const handleSubmit = async (e) => {
+    // 阻擋表單預設送出行為
+    e.preventDefault()
+
+    // 表單檢查---START---
+    // 建立一個新的錯誤訊息物件
+    const newErrors = { email: '', password: '' }
+
+    // if (user.username === '') {
+    // 上面寫法常見改為下面這樣，`if(user.username)` 代表有填寫，
+    // 所以反相判斷 `if(!user.username)` 代表沒填寫
+    if (!user.email) {
+      newErrors.email = '帳號為必填'
+    }
+
+    if (user.password && user.password.length < 6) {
+      newErrors.password = '密碼至少6個字元'
+    }
+
+    if (user.password === '') {
+      newErrors.password = '密碼為必填'
+    }
+
+    // if (user.password2 === '') {
+    //   newErrors.password2 = '確認密碼為必填'
+    // }
+
+    // if (user.password !== user.password2) {
+    //   newErrors.password = '密碼與確認密碼需要相同'
+    //   newErrors.password2 = '密碼與確認密碼需要相同'
+    // }
+
+    // 檢查完設定到狀態中
+    setErrors(newErrors)
+
+    // 物件屬性值中有非空白字串時，代表有錯誤發生
+    const hasErrors = Object.values(newErrors).some((v) => v)
+
+    // 有錯誤發生，不送到伺服器去
+    if (hasErrors) {
+      return // 函式中作流程控制，會跳出函式不執行之後的程式碼
+    }
+    // 表單檢查--- END ---
+
+    // 檢查沒問題後再送到伺服器
+    const res = await fetch('http://localhost:3005/api/user/login', {
+      credentials: 'include', // 設定cookie或是要存取隱私資料時帶cookie到伺服器一定要加
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+
+    const data = await res.json()
+
+    if (data.status === 'success') {
+      alert('登入成功')
+      const returnUser = parseJwt(data.data.accessToken)
+      console.log(returnUser)
+    } else {
+      alert(data.message)
+    }
+  }
+
+  const handleLogout = async () => {
+    // 檢查沒問題後再送到伺服器
+    const res = await fetch('http://localhost:3005/api/members/logout', {
+      credentials: 'include', // 設定cookie或是要存取隱私資料時帶cookie到伺服器一定要加
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({}),
+    })
+
+    const data = await res.json()
+
+    if (data.status === 'success') {
+      alert('登出成功')
+    } else {
+      alert(data.message)
+    }
+  }
+
+  const handleCheck = async () => {
+    // 檢查沒問題後再送到伺服器
+    const res = await fetch('http://localhost:3005/api/members/check', {
+      credentials: 'include', // 設定cookie或是要存取隱私資料時帶cookie到伺服器一定要加
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+
+    const data = await res.json()
+
+    if (data.status === 'success') {
+      console.log(data.data)
+    } else {
+      alert(data.message)
+    }
+  }
+
   return (
     <>
       <section className={styles['section']}>
-        <Headers />
+        <Header />
         <p className={styles['tip']}>點選圖片中的按鈕以切換登入、註冊</p>
         <div className={styles['cont']}>
-          <div className={`${styles['form']} ${styles['sign-in']}`}>
+          <form
+            className={`${styles['form']} ${styles['sign-in']}`}
+            onSubmit={handleSubmit}
+          >
             <h2 className={styles['h2']}>立即參觀！</h2>
             <label className={styles['label']}>
               <span className={`${styles['span']} ${styles['spanl']}`}>
-                Email：
+                Email：{' '}
               </span>
-              <input className={styles['input']} type="email" />
+              <input
+                className={styles['input']}
+                type="email"
+                name="email"
+                value={user.email}
+                onChange={handleFieldChange}
+              />
             </label>
+            <div className="error">{errors.username} </div>
             <label className={styles['label']}>
               <span className={`${styles['span']} ${styles['spanl']}`}>
-                密碼：
+                密碼：{' '}
               </span>
-              <input className={styles['input']} type="password" />
+              <input
+                className={styles['input']}
+                type={showPassword ? 'text' : 'password'}
+                name="password"
+                value={user.password}
+                onChange={handleFieldChange}
+              />
             </label>
+            <input
+              type="checkbox"
+              checked={showPassword}
+              onChange={(e) => {
+                setShowPassword(e.target.checked)
+              }}
+            />
+            顯示密碼
+            <div className="error">{errors.password}</div>
             <p className={styles['forgot-pass']}>忘記密碼？</p>
             <button
-              type="button"
+              type="submit"
               className={`${styles['button']} ${styles['submit']}`}
             >
               登入
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                setUser({
+                  email: 'Hyunwoo01@gmail.com',
+                  password: 'Pa55w.rd01',
+                })
+              }}
+            >
+              一鍵填入
+            </button>
+            <button type="button" onClick={handleCheck}>
+              檢查登入狀況
+            </button>
+            <button type="button" onClick={handleLogout}>
+              登出
             </button>
             <button
               type="button"
@@ -57,7 +240,7 @@ export default function LoginRegister() {
                 Google
               </span>
             </button>
-          </div>
+          </form>
           <div className={styles['sub-cont']}>
             <div className={styles['img']}>
               <di className={`${styles['img__text']} ${styles['m--up']}`}>
@@ -112,7 +295,7 @@ export default function LoginRegister() {
                 <input className={styles['input']} type="password" />
               </label>
               <button
-                type="button"
+                type="submit"
                 className={`${styles['button']} ${styles['submit']}`}
               >
                 註冊
@@ -129,6 +312,7 @@ export default function LoginRegister() {
             </div>
           </div>
         </div>
+        <Footer />
       </section>
     </>
   )
