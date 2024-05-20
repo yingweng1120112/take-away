1f5f5261f129583b4a52d95d29f9f25eb6b79dfb
YHT <andy17961796@gmail.com>
2024-05-20 23:41:43 +0800

    連接資料庫 增加商品圖檔

--- server/routes/products.js
+++ server/routes/products.js
@@ -5,29 +5,35 @@ const router = express.Router()
 import { getIdParam } from '#db-helpers/db-tool.js'
 
 // 資料庫使用
-// import sequelize from '#configs/db.js'
-// const { product } = sequelize.models
+import sequelize from '#configs/db.js'
+const { Product } = sequelize.models
+
+import db from '#configs/mysql.js'
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Products' })
-})
+// router.get('/', function (req, res, next) {
+//   res.render('index', { title: 'Products' })
+// })
 
 // GET - 得到所有商品資料
-// router.get('/', async function (req, res) {
-//   const products = await product.findAll({ logging: console.log })
-//   // 處理如果沒找到資料
+router.get('/', async function (req, res) {
+  // const products = await Product.findAll({ logging: console.log })
 
-//   // 標準回傳JSON
-//   return res.json({ status: 'success', data: { products } })
-// })
+  const [rows] = await db.query('SELECT * FROM product')
+  const products = rows
+
+  // 處理如果沒找到資料
+
+  // 標準回傳JSON
+  return res.json({ status: 'success', data: { products } })
+})
 
 // GET - 得到單筆資料(注意，有動態參數時要寫在GET區段最後面)
 router.get('/:id', async function (req, res) {
   // 轉為數字
   const id = getIdParam(req)
 
-  const user = await User.findByPk(id, {
+  const user = await Product.findByPk(id, {
     raw: true, // 只需要資料表中資料
   })
 
