1d2ea1891929ac476be8ad6064d871abf6e161d0
yushanys <lightful2013@gmail.com>
2024-05-22 21:52:29 +0800

    首頁加上產品推薦

--- client/pages/index.js
+++ client/pages/index.js
@@ -4,18 +4,16 @@ import Image from 'next/image'
 import PlaceholderText from '@/components/common/placeholder-text'
 import Header from '@/components/layout/header'
 import Footer from '@/components/layout/footer'
+import ProductCarousel from '@/components/shopping-cart/carousel'
 
 export default function Home() {
   return (
     <>
       <Header />
-      <h1 className="mb-3 display-5 fw-bold text-body-emphasis">
-        Next + Bootstrap5 範例
-      </h1>
 
       <div className="px-4 pt-5 my-5 text-center border-bottom">
         <h1 className="display-4 fw-bold text-body-emphasis">
-          Centered screenshot
+          其他部分
         </h1>
         <div className="col-lg-6 mx-auto">
           <p className="lead mb-4">
@@ -25,18 +23,6 @@ export default function Home() {
             extensive prebuilt components, and powerful JavaScript plugins.
           </p>
           <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-            <button
-              type="button"
-              className="btn btn-primary btn-lg px-4 me-sm-3"
-            >
-              Primary button
-            </button>
-            <button
-              type="button"
-              className="btn btn-outline-secondary btn-lg px-4"
-            >
-              Secondary
-            </button>
           </div>
         </div>
         <div className="overflow-hidden" style={{ maxHeight: '30vh' }}>
@@ -52,168 +38,31 @@ export default function Home() {
           </div>
         </div>
       </div>
-      {/* <div className="container col-xxl-10 px-1 py-2">
-        <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-          <div className="col-10 col-sm-8 col-lg-6">
-            <Image
-              src="/images/heroes/bootstrap-themes.png"
-              className="d-block mx-lg-auto img-fluid"
-              alt="Bootstrap Themes"
-              width="700"
-              height="500"
-              loading="lazy"
-            />
-          </div>
-          <div className="col-lg-6">
-            <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">
-              Responsive left-aligned hero with image
-            </h1>
-            <p className="lead">
-              Quickly design and customize responsive mobile-first sites with
-              Bootstrap, the world’s most popular front-end open source toolkit,
-              featuring Sass variables and mixins, responsive grid system,
-              extensive prebuilt components, and powerful JavaScript plugins.
-            </p>
-            <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-              <button
-                type="button"
-                className="btn btn-primary btn-lg px-4 me-md-2"
-              >
-                Primary
-              </button>
-              <button
-                type="button"
-                className="btn btn-outline-secondary btn-lg px-4"
-              >
-                Default
-              </button>
-            </div>
-          </div>
-        </div>
+      <div className="commend">
+        <h3 className="commendtitle">產品推薦</h3>
+        <ProductCarousel />
       </div>
-      <div className="container px-4 py-5" id="custom-cards">
-        <h2 className="pb-2 border-bottom">Custom cards</h2>
 
-        <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
-          <div className="col">
-            <div
-              className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
-              style={{
-                backgroundImage: "url('/images/features/unsplash-photo-1.jpg')",
-              }}
-            >
-              <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
-                  Short title, long jacket
-                </h3>
-                <ul className="d-flex list-unstyled mt-auto">
-                  <li className="me-auto">
-                    <img
-                      src="https://github.com/twbs.png"
-                      alt="Bootstrap"
-                      width="32"
-                      height="32"
-                      className="rounded-circle border border-white"
-                    />
-                  </li>
-                  <li className="d-flex align-items-center me-3 ">
-                    <i className="bi bi-geo-fill me-2"></i>
-                    <small>Earth</small>
-                  </li>
-                  <li className="d-flex align-items-center">
-                    <i className="bi bi-calendar3 me-2"></i>
-                    <small>3d</small>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
-          <div className="col">
-            <div
-              className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
-              style={{
-                backgroundImage: "url('/images/features/unsplash-photo-2.jpg')",
-              }}
-            >
-              <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
-                  Much longer title that wraps to multiple lines
-                </h3>
-                <ul className="d-flex list-unstyled mt-auto">
-                  <li className="me-auto">
-                    <img
-                      src="https://github.com/twbs.png"
-                      alt="Bootstrap"
-                      width="32"
-                      height="32"
-                      className="rounded-circle border border-white"
-                    />
-                  </li>
-                  <li className="d-flex align-items-center me-3">
-                    <i className="bi bi-geo-fill me-2"></i>
-                    <small>Pakistan</small>
-                  </li>
-                  <li className="d-flex align-items-center">
-                    <i className="bi bi-calendar3 me-2"></i>
-                    <small>4d</small>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-
-          <div className="col">
-            <div
-              className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
-              style={{
-                backgroundImage: "url('/images/features/unsplash-photo-3.jpg')",
-              }}
-            >
-              <div className="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
-                <h3 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">
-                  Another longer title belongs here
-                </h3>
-                <ul className="d-flex list-unstyled mt-auto">
-                  <li className="me-auto">
-                    <img
-                      src="https://github.com/twbs.png"
-                      alt="Bootstrap"
-                      width="32"
-                      height="32"
-                      className="rounded-circle border border-white"
-                    />
-                  </li>
-                  <li className="d-flex align-items-center me-3">
-                    <i className="bi bi-geo-fill me-2"></i>
-                    <small>California</small>
-                  </li>
-                  <li className="d-flex align-items-center">
-                    <i className="bi bi-calendar3 me-2"></i>
-                    <small>5d</small>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
       <style global jsx>
         {`
-          .card-cover {
-            background-repeat: no-repeat;
-            background-position: center center;
-            background-size: cover;
+          .commend {
+            background-image: url(/shopping-cart/commendbg.png);
+            background-size: contain;
+            margin: 0;
+            height: 37rem;
           }
+          .commendtitle {
+            
+            padding-top: 35px;
+            text-align: center;
+            letter-spacing: 10px;
+            color: var(--deep-gray);
+            font-weight: 600;
+          }
+          @media screen and (max-width: 414px) {
+            .commend {
+            height: 25rem;
 
-          .text-shadow-1 {
-            text-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.25);
-          }
-          .text-shadow-2 {
-            text-shadow: 0 0.25rem 0.5rem rgba(0, 0, 0, 0.25);
-          }
-          .text-shadow-3 {
-            text-shadow: 0 0.5rem 1.5rem rgba(0, 0, 0, 0.25);
           }
         `}
       </style>
