36b4a4f7b5a87b737c14a8e9b915d2d1b9016d04
larry <asd109150@gmail.com>
2024-05-23 15:31:40 +0800

    INDEX輪播

--- client/pages/index.js
+++ client/pages/index.js
@@ -4,54 +4,16 @@ import Image from 'next/image'
 import PlaceholderText from '@/components/common/placeholder-text'
 import Header from '@/components/layout/header'
 import Footer from '@/components/layout/footer'
-
+import Swiper from '@/components/swiper/indexSwiper'
+import styles from '@/styles/swiper/indexSwipper.module.css'
 export default function Home() {
   return (
     <>
       <Header />
-      <h1 className="mb-3 display-5 fw-bold text-body-emphasis">
-        Next + Bootstrap5 範例
-      </h1>
+      <div    className={styles['inswipper']}>
+      <Swiper />
+    </div>
 
-      <div className="px-4 pt-5 my-5 text-center border-bottom">
-        <h1 className="display-4 fw-bold text-body-emphasis">
-          Centered screenshot
-        </h1>
-        <div className="col-lg-6 mx-auto">
-          <p className="lead mb-4">
-            Quickly design and customize responsive mobile-first sites with
-            Bootstrap, the world’s most popular front-end open source toolkit,
-            featuring Sass variables and mixins, responsive grid system,
-            extensive prebuilt components, and powerful JavaScript plugins.
-          </p>
-          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-            <button
-              type="button"
-              className="btn btn-primary btn-lg px-4 me-sm-3"
-            >
-              Primary button
-            </button>
-            <button
-              type="button"
-              className="btn btn-outline-secondary btn-lg px-4"
-            >
-              Secondary
-            </button>
-          </div>
-        </div>
-        <div className="overflow-hidden" style={{ maxHeight: '30vh' }}>
-          <div className="container px-5">
-            <Image
-              src="/images/heroes/bootstrap-docs.png"
-              className="img-fluid border rounded-3 shadow-lg mb-4"
-              alt="Example image"
-              width="700"
-              height="500"
-              loading="lazy"
-            />
-          </div>
-        </div>
-      </div>
       {/* <div className="container col-xxl-10 px-1 py-2">
         <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
           <div className="col-10 col-sm-8 col-lg-6">
