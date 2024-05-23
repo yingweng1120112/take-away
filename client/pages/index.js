672954bb89015fcabd50c4c3a775b97058ca7d85
yingweng <yingweng1120112@gmail.com>
2024-05-23 15:55:19 +0800

    修改index

--- client/pages/index.js
+++ client/pages/index.js
@@ -4,73 +4,26 @@ import Image from 'next/image'
 import PlaceholderText from '@/components/common/placeholder-text'
 import Header from '@/components/layout/header'
 import Footer from '@/components/layout/footer'
-<<<<<<< HEAD
 import Swiper from '@/components/swiper/indexSwiper'
 import styles from '@/styles/swiper/indexSwipper.module.css'
-=======
 import ProductCarousel from '@/components/shopping-cart/carousel'
 import IndexService from '@/components/index-service/indexservice'
 
->>>>>>> 3a688093e85610a719e424fb07bd6beb24f42c5e
 export default function Home() {
   return (
     <>
       <Header />
-<<<<<<< HEAD
-      <div    className={styles['inswipper']}>
-      <Swiper />
-    </div>
-
-      {/* <div className="container col-xxl-10 px-1 py-2">
-        <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-          <div className="col-10 col-sm-8 col-lg-6">
-            <Image
-              src="/images/heroes/bootstrap-themes.png"
-              className="d-block mx-lg-auto img-fluid"
-              alt="Bootstrap Themes"
-              width="700"
-              height="500"
-              loading="lazy"
-            />
-          </div>
-          <div className="col-lg-6">
-            <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">
-              Responsive left-aligned hero with image
-            </h1>
-            <p className="lead">
-              Quickly design and customize responsive mobile-first sites with
-              Bootstrap, the world’s most popular front-end open source toolkit,
-              featuring Sass variables and mixins, responsive grid system,
-              extensive prebuilt components, and powerful JavaScript plugins.
-            </p>
-            <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-              <button
-                type="button"
-                className="btn btn-primary btn-lg px-4 me-md-2"
-              >
-                Primary
-              </button>
-              <button
-                type="button"
-                className="btn btn-outline-secondary btn-lg px-4"
-              >
-                Default
-              </button>
-            </div>
-          </div>
-        </div>
+      <div className={styles['inswipper']}>
+        <Swiper />
       </div>
       <div className="container px-4 py-5" id="custom-cards">
         <h2 className="pb-2 border-bottom">Custom cards</h2>
-=======
-    <IndexService />
-
-
+        <IndexService />
+      </div>
       <div className="commend">
         <h3 className="commendtitle">產品推薦</h3>
         <ProductCarousel />
       </div>
->>>>>>> 3a688093e85610a719e424fb07bd6beb24f42c5e
 
       <style global jsx>
         {`
@@ -91,7 +44,6 @@ export default function Home() {
           @media screen and (max-width: 414px) {
             .commend {
             height: 25rem;
-
           }
         `}
       </style>
