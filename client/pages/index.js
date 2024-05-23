3a688093e85610a719e424fb07bd6beb24f42c5e
yingweng <yingweng1120112@gmail.com>
2024-05-23 15:35:53 +0800

    新增indexservice

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,39 +5,15 @@ import PlaceholderText from '@/components/common/placeholder-text'
 import Header from '@/components/layout/header'
 import Footer from '@/components/layout/footer'
 import ProductCarousel from '@/components/shopping-cart/carousel'
+import IndexService from '@/components/index-service/indexservice'
 
 export default function Home() {
   return (
     <>
       <Header />
+    <IndexService />
 
-      <div className="px-4 pt-5 my-5 text-center border-bottom">
-        <h1 className="display-4 fw-bold text-body-emphasis">
-          其他部分
-        </h1>
-        <div className="col-lg-6 mx-auto">
-          <p className="lead mb-4">
-            Quickly design and customize responsive mobile-first sites with
-            Bootstrap, the world’s most popular front-end open source toolkit,
-            featuring Sass variables and mixins, responsive grid system,
-            extensive prebuilt components, and powerful JavaScript plugins.
-          </p>
-          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
-          </div>
-        </div>
-        <div className="overflow-hidden" style={{ maxHeight: '30vh' }}>
-          <div className="container px-5">
-            <Image
-              src="/images/heroes/bootstrap-docs.png"
-              className="img-fluid border rounded-3 shadow-lg mb-4"
-              alt="Example image"
-              width="700"
-              height="500"
-              loading="lazy"
-            />
-          </div>
-        </div>
-      </div>
+
       <div className="commend">
         <h3 className="commendtitle">產品推薦</h3>
         <ProductCarousel />
