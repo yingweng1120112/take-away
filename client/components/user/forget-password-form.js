1aa99df935f507c096be4be9c7cae71e8b856f0d
yingweng <yingweng1120@gmail.com>
2024-06-01 17:33:58 +0800

    新增忘記密碼小眼睛

--- client/components/user/forget-password-form.js
+++ client/components/user/forget-password-form.js
@@ -5,8 +5,8 @@ import Link from 'next/link'
 import Header from '@/components/layout/header'
 import Footer from '@/components/layout/footer'
 import { FaEye, FaEyeSlash } from 'react-icons/fa'
-import { ToastContainer, toast, Slide } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast, Slide } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export default function ForgetPasswordForm() {
   const [email, setEmail] = useState('')
@@ -15,42 +15,48 @@ export default function ForgetPasswordForm() {
   const [confirmPassword, setConfirmPassword] = useState('')
   const [message, setMessage] = useState('')
 
+  const [showPassword, setShowPassword] = useState(false)
+  const [showPassword2, setShowPassword2] = useState(false)
+
   const handleGetverification_code = async () => {
     // 請求後端發送驗證碼
     try {
-      const response = await fetch('http://localhost:3005/api/forgot-password/send-code', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email })
-      })
+      const response = await fetch(
+        'http://localhost:3005/api/forgot-password/send-code',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email }),
+        }
+      )
       const result = await response.json()
-      toast.success((result.message), {
-        position: "top-center",
+      toast.success(result.message, {
+        position: 'top-center',
         autoClose: 600,
         hideProgressBar: true,
         closeOnClick: true,
         pauseOnHover: false,
         draggable: false,
         progress: undefined,
-        theme: "dark",
+        theme: 'dark',
         transition: Slide,
-      });
+      })
       // setMessage(result.message)
     } catch (error) {
       // setMessage('發送驗證碼失敗')
-      toast.success(('發送驗證碼失敗'), {
-        position: "top-center",
+      toast.success('發送驗證碼失敗', {
+        position: 'top-center',
         autoClose: 600,
         hideProgressBar: true,
         closeOnClick: true,
         pauseOnHover: false,
         draggable: false,
         progress: undefined,
-        theme: "dark",
+        theme: 'dark',
         transition: Slide,
-      });
+      })
     }
   }
 
@@ -58,55 +64,58 @@ export default function ForgetPasswordForm() {
     event.preventDefault()
     if (password !== confirmPassword) {
       // setMessage('密碼和確認密碼不一致')
-      toast.success(('密碼和確認密碼不一致'), {
-        position: "top-center",
+      toast.success('密碼和確認密碼不一致', {
+        position: 'top-center',
         autoClose: 600,
         hideProgressBar: true,
         closeOnClick: true,
         pauseOnHover: false,
         draggable: false,
         progress: undefined,
-        theme: "dark",
+        theme: 'dark',
         transition: Slide,
-      });
+      })
       return
     }
 
     // 請求後端重設密碼
     try {
-      const response = await fetch('http://localhost:3005/api/forgot-password/reset', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email, verification_code, password })
-      })
+      const response = await fetch(
+        'http://localhost:3005/api/forgot-password/reset',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, verification_code, password }),
+        }
+      )
       const result = await response.json()
       // setMessage(result.message)
-      toast.success((result.message), {
-        position: "top-center",
+      toast.success(result.message, {
+        position: 'top-center',
         autoClose: 600,
         hideProgressBar: true,
         closeOnClick: true,
         pauseOnHover: false,
         draggable: false,
         progress: undefined,
-        theme: "dark",
+        theme: 'dark',
         transition: Slide,
-      });
+      })
     } catch (error) {
       // setMessage('重設密碼失敗')
-      toast.success(('重設密碼失敗'), {
-        position: "top-center",
+      toast.success('重設密碼失敗', {
+        position: 'top-center',
         autoClose: 600,
         hideProgressBar: true,
         closeOnClick: true,
         pauseOnHover: false,
         draggable: false,
         progress: undefined,
-        theme: "dark",
+        theme: 'dark',
         transition: Slide,
-      });
+      })
     }
   }
 
@@ -114,7 +123,7 @@ export default function ForgetPasswordForm() {
     <section className={styles['section']}>
       <Header />
       <div className={styles['cont']}>
-      <img className={styles['login']} src="\img\user\login.svg" alt="" />
+        <img className={styles['login']} src="\img\user\login.svg" alt="" />
         <form className={`${styles['form']}`} onSubmit={handleResetPassword}>
           <div className="row mb-3">
             <div className="col-sm-12">
@@ -146,28 +155,45 @@ export default function ForgetPasswordForm() {
             </div>
           </div>
           <div className="row mb-3">
-            <div className="col-sm-12">
+            <div className="col-sm-12 d-flex">
               <input
-                type="password"
+                type={showPassword ? 'text' : 'password'}
                 className={`form-control w-100 ${styles['form-control']}`}
                 placeholder="密碼"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
+              <span
+                className={`eye ms-2 d-flex align-items-center`}
+                onClick={() => setShowPassword(!showPassword)}
+                style={{ cursor: 'pointer' }}
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </span>
             </div>
           </div>
           <div className="row mb-3">
-            <div className="col-sm-12">
+            <div className="col-sm-12 d-flex">
               <input
-                type="password"
+                type={showPassword2 ? 'text' : 'password'}
                 className={`form-control w-100 ${styles['form-control']}`}
                 placeholder="確認密碼"
                 value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
               />
+              <span
+                className={`eye ms-2 d-flex align-items-center`}
+                onClick={() => setShowPassword2(!showPassword2)}
+                style={{ cursor: 'pointer' }}
+              >
+                {showPassword2 ? <FaEyeSlash /> : <FaEye />}
+              </span>
             </div>
           </div>
-          <button type="submit" className={`${login['button']} ${login['submit']}`} >
+          <button
+            type="submit"
+            className={`${login['button']} ${login['submit']}`}
+          >
             確定
           </button>
           {message && <p className="text-center mt-3">{message}</p>}
@@ -180,6 +206,15 @@ export default function ForgetPasswordForm() {
         </form>
         <img className={styles['login2']} src="\img\user\login2.svg" alt="" />
       </div>
+      <style jsx>
+        {`
+          .eye {
+            position: relative;
+            left: -10%;
+            bottom: 1%;
+          }
+        `}
+      </style>
       <Footer />
     </section>
   )
